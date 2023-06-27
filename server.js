efb5644d84cb5f49fa588ac1abe619303a230d66
niveshpadmakumars <niveshpadmakumar.s@gmail.com>
2023-06-27 14:52:57 +0530

    middleware

--- server.js
+++ server.js
@@ -10,12 +10,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+const authenticateFields = (req, res, next) => {
+  const { email, subject, content } = req.body;
+
+  if (!email || !subject || !content) {
+    return res.status(400).send('Missing required fields.');
+  }
+  next();
+};
+
+app.post('/send-email', authenticateFields, controller.sendEmail) 
+
 //path defining - frontend
 app.get('/', (req, res,) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));  //route handling
 });
 
-app.post('/send-email', controller.sendEmail); //route handling
+//app.post('/send-email', controller.sendEmail); 
 
 //server starting
 app.listen(port, () => {
