d457e2252653016419f494ddaa70478e81b48f17
niveshpadmakumars <niveshpadmakumar.s@gmail.com>
2023-06-28 15:03:38 +0530

    XprsJS

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const controller = require('./Controller/controller');
+//const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static('public'));
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const authenticateFields = (req, res, next) => {
+const auth = (req, res, next) => {
   const { email, subject, content } = req.body;
   if (!email || !subject || !content) {
     return res.status(400).send('enter the required fields.');
   }
   next();
 };
-app.post('/send-email', authenticateFields, controller.sendEmail) 
+app.post('/send-email', auth, controller.sendEmail) 
 
 //path defining - frontend
 app.get('/', (req, res,) => {
